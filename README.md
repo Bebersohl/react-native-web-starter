a0b0803433085db55c10eff38e038bd843024ed8
Brandon Ebersohl <brandon.ebersohl@bestbuy.com>
2019-02-04 15:26:59 -0600

    updated index

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# react-native-web-starter
-Starting template for react-native-web projects. Built with create-react-app and expo-cli
+# easy-pack
+TODO
--- src/index.native.js
+++ src/index.native.js
@@ -1,7 +1,7 @@
 import {registerRootComponent} from 'expo';
 // import React from 'react';
 // import { View } from 'react-native';
-import App from './App'
+import App from './components/App'
 
 registerRootComponent(App);
 
